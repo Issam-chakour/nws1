8b163514ba06adb85f74fb8475384ab7e87d5088
Issam-chakour <issam.chakour2@gmail.com>
2021-07-13 13:23:11 +0100

    ok FRANT END

--- phpfile/others_news.php
+++ phpfile/others_news.php
@@ -7,94 +7,73 @@ $db = mysqli_connect('localhost', 'root', '', 'newsmar'); ?>
 <html lang="en">
 
 <head>
-	<title>Website menu 07</title>
+	<title> home </title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<link href='https://fonts.googleapis.com/css?family=Roboto:400,100,300,700' rel='stylesheet' type='text/css'>
 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-
-	<link rel="stylesheet" href="../css/style.css">
+	
+  <link rel="stylesheet" href="../css/style.css">
 
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css"
 		integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
-
 </head>
 
 <body dir="rtl">
-	<section class="ftco-section">
-		<div class="container">
-			<div class="row justify-content-center">
-				<div class="col-md-6 text-center mb-5">
-					<h2 class="heading-section">NEWS</h2>
-				</div>
-			</div>
-		</div>
+	<div>
+	<h1>NEWS</h1>
+	</div>
 
-		<div class="container-fluid px-md-5">
-			<div class="row justify-content-between">
-				<div class="col-md-8 order-md-last">
-					<div class="row">
-						<div class="col-md-6 d-md-flex justify-content-end mb-md-0 mb-3">
-					
-						</div>
-					</div>
-				</div>
+<nav class="navbar navbar-expand-lg navbar-dark" id="nav-bg" >
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
 
-			</div>
-		</div>
-		<nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
-			<div class="container-fluid">
 
-				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
-					aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="fa fa-bars"></span> Menu
-				</button>
-				<div class="collapse navbar-collapse" id="ftco-nav">
-					<ul class="navbar-nav">
-
-						<li class="nav-item "><a href="homepage.php" class="nav-link">الرئيسية</a></li>
-
-						<li class="nav-item"><a class="nav-link" href="nat_news.php" id="dropdown04">الأخبار الوطنية</a></li>
-						<li class="nav-item"><a href="int_news.php" class="nav-link"> الأخبار الدولية </a></li>
-						<li class="nav-item"><a href="sports_news.php" class="nav-link">الأخبار الرياضية</a></li>
-						<li class="nav-item active"><a href="others_news.php" class="nav-link">أخرى</a></li>
-						<li class="nav-item"><a href="about.php" class="nav-link">من نحن</a></li>
-						<li class="nav-item"><a href="callus.php" class="nav-link">اتصل بنا</a></li>
-						<li class="nav-item"><a href="#" class="nav-link">COVID-19</a></li>
-						<!-- SOCIAL MEDIA -->
-						<li>
-							<div class="col-md-4 d-flex">
-								<div class="social-media">
-									<p class="d-flex">
-										<a href="#" class="d-flex align-items-center justify-content-center "><span
-												class="fa fa-facebook "><i class="sr-only">Facebook</i></span></a>
-										<a href="#" class="d-flex align-items-center justify-content-center"><span
-												class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-										<a href="#" class="d-flex align-items-center justify-content-center"><span
-												class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-										<a href="#" class="d-flex align-items-center justify-content-center"><span
-												class="fa fa-youtube"><i class="sr-only">Dribbble</i></span></a>
-									</p>
-								</div>
-							</div>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-		<!-- END nav -->
-
-	</section>
-	<!-- search -->
-	<form action="#" class="searchform order-lg-last">
-		<div class="form-group d-flex">
-			<input type="text" class="form-control pl-3" placeholder="Search">
-			<button type="submit" placeholder="" class="form-control search"><span
-					class="fa fa-search"></span></button>
-		</div>
-	</form>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+    <ul class="navbar-nav  mt-2 mt-lg-0" id="mynav">
+     
+	  <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle " href="#" id="navbarScrollingDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+		الأخبار
+        </a>
+        <ul class="dropdown-menu " aria-labelledby="navbarScrollingDropdown">
+		<li><a class="dropdown-item " href="homepage.php">الرئيسية </a></li>
+          <li><a class="dropdown-item " href="nat_news.php"> الأخبار الوطنية </a></li>
+          <li><a class="dropdown-item  " href="int_news.php"> الأخبار الدولية </a></li>
+          <li><a class="dropdown-item " href="sports_news.php"> الأخبار الرياضية </a></li>
+		  <li><a class="dropdown-item active" href="others_news.php">  أخرى </a></li>
+        </ul>
+	  </li>
+	  <li class="nav-item ">
+        <a class="nav-link ml-2" href="about.php"> من نحن </a>
+      </li>
+	  <li class="nav-item ">
+        <a class="nav-link ml-2" href="callus.php">اتصل بنا </a>
+      </li>
+	  <li class="nav-item ">
+        <a class="nav-link ml-2" href="#">COVID-19 </a>
+      </li>
+     
+	  <div class="col-md-2 d-flex" >
+	<div class="social-media">
+		<p class="d-flex" id="nav-icon" >
+			<a href="#" class="d-flex align-items-center justify-content-center "><span
+					class="fa fa-facebook "><i class="sr-only">Facebook</i></span></a>
+			<a href="#" class="d-flex align-items-center justify-content-center"><span
+					class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
+			<a href="#" class="d-flex align-items-center justify-content-center"><span
+					class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
+			<a href="#" class="d-flex align-items-center justify-content-center"><span
+					class="fa fa-youtube"><i class="sr-only">youtube</i></span></a>
+		</p>
+	</div>
+</div>
+    </ul>
+  </div>
+</nav>
     <!-- end   navbar -->
 
 
